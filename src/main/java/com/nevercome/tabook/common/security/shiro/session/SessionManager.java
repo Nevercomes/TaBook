562616f6273886b09276d654cb6a2bb0af74d48f
Nevercome <sunss1903@163.com>
2019-04-03 22:46:57 +0800

    add JedisSessionDao

--- src/main/java/com/nevercome/tabook/common/security/shiro/session/SessionManager.java
+++ src/main/java/com/nevercome/tabook/common/security/shiro/session/SessionManager.java
@@ -11,7 +11,9 @@ import org.apache.shiro.web.servlet.Cookie;
 import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
 import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.apache.shiro.web.session.mgt.WebSessionKey;
 import org.apache.shiro.web.util.WebUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -23,6 +25,7 @@ import java.util.Date;
 
 /**
  * 自定义WEB会话管理类
+ *
  * @author ThinkGem
  * @version 2014-7-20
  */
@@ -35,15 +38,17 @@ public class SessionManager extends DefaultWebSessionManager {
     @Override
     protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
         // 如果参数中包含“__sid”参数，则使用此sid会话。 例如：http://localhost/project?__sid=xxx&__cookie=true
+        // sun MiniApplet也是可以保存Headers的可能不需要这样
         String sid = request.getParameter("__sid");
         if (StringUtils.isNotBlank(sid)) {
             // 是否将sid保存到cookie，浏览器模式下使用此参数。
-            if (WebUtils.isTrue(request, "__cookie")){
-                HttpServletRequest rq = (HttpServletRequest)request;
-                HttpServletResponse rs = (HttpServletResponse)response;
+            if (WebUtils.isTrue(request, "__cookie")) {
+                HttpServletRequest rq = (HttpServletRequest) request;
+                HttpServletResponse rs = (HttpServletResponse) response;
                 Cookie template = getSessionIdCookie();
                 Cookie cookie = new SimpleCookie(template);
-                cookie.setValue(sid); cookie.saveTo(rq, rs);
+                cookie.setValue(sid);
+                cookie.saveTo(rq, rs);
             }
             // 设置当前session状态
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE,
@@ -51,7 +56,7 @@ public class SessionManager extends DefaultWebSessionManager {
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, sid);
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
             return sid;
-        }else{
+        } else {
             return super.getSessionId(request, response);
         }
     }
@@ -62,122 +67,122 @@ public class SessionManager extends DefaultWebSessionManager {
     }
 
     protected Session retrieveSession(SessionKey sessionKey) {
-        try{
+        try {
             return super.retrieveSession(sessionKey);
-        }catch (UnknownSessionException e) {
+        } catch (UnknownSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public Date getStartTimestamp(SessionKey key) {
-        try{
+        try {
             return super.getStartTimestamp(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public Date getLastAccessTime(SessionKey key) {
-        try{
+        try {
             return super.getLastAccessTime(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
-    public long getTimeout(SessionKey key){
-        try{
+    public long getTimeout(SessionKey key) {
+        try {
             return super.getTimeout(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return 0;
         }
     }
 
     public void setTimeout(SessionKey key, long maxIdleTimeInMillis) {
-        try{
+        try {
             super.setTimeout(key, maxIdleTimeInMillis);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
         }
     }
 
     public void touch(SessionKey key) {
-        try{
+        try {
             super.touch(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
         }
     }
 
     public String getHost(SessionKey key) {
-        try{
+        try {
             return super.getHost(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public Collection<Object> getAttributeKeys(SessionKey key) {
-        try{
+        try {
             return super.getAttributeKeys(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public Object getAttribute(SessionKey sessionKey, Object attributeKey) {
-        try{
+        try {
             return super.getAttribute(sessionKey, attributeKey);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public void setAttribute(SessionKey sessionKey, Object attributeKey, Object value) {
-        try{
+        try {
             super.setAttribute(sessionKey, attributeKey, value);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
         }
     }
 
     public Object removeAttribute(SessionKey sessionKey, Object attributeKey) {
-        try{
+        try {
             return super.removeAttribute(sessionKey, attributeKey);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
             return null;
         }
     }
 
     public void stop(SessionKey key) {
-        try{
+        try {
             super.stop(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
         }
     }
 
     public void checkValid(SessionKey key) {
-        try{
+        try {
             super.checkValid(key);
-        }catch (InvalidSessionException e) {
+        } catch (InvalidSessionException e) {
             // 获取不到SESSION不抛出异常
         }
     }
 
     @Override
     protected Session doCreateSession(SessionContext context) {
-        try{
+        try {
             return super.doCreateSession(context);
-        }catch (IllegalStateException e) {
+        } catch (IllegalStateException e) {
             return null;
         }
     }
@@ -191,9 +196,9 @@ public class SessionManager extends DefaultWebSessionManager {
 
     @Override
     public Session start(SessionContext context) {
-        try{
+        try {
             return super.start(context);
-        }catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             SimpleSession session = new SimpleSession();
             session.setId(0);
             return session;
---
+++ src/main/java/com/nevercome/tabook/modules/sys/web/IdentifyController.java
@@ -0,0 +1,15 @@
+package com.nevercome.tabook.modules.sys.web;
+
+import com.nevercome.tabook.common.web.BaseController;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 实名认证Controller
+ * @Author: sun
+ * @Description:
+ * @Date: 2019/4/3
+ */
+@Controller
+public class IdentifyController extends BaseController {
+
+}

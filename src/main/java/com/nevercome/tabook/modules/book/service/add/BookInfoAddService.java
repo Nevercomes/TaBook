f997bc3cacde7f48690a9eac8b83a26f6903fb3c
Nevercome <sunss1903@163.com>
2019-04-26 17:06:41 +0800

    BookInfo add tag and sort

--- src/main/java/com/nevercome/tabook/modules/book/service/add/BookInfoAddService.java
+++ src/main/java/com/nevercome/tabook/modules/book/service/add/BookInfoAddService.java
@@ -4,8 +4,14 @@ import com.nevercome.tabook.common.service.CrudService;
 import com.nevercome.tabook.common.utils.StringUtils;
 import com.nevercome.tabook.modules.book.dao.add.BookInfoAddDao;
 import com.nevercome.tabook.modules.book.entity.add.BookInfoAdd;
+import com.nevercome.tabook.modules.book.entity.info.BookInfoClass;
 import com.nevercome.tabook.modules.book.entity.info.BookInfoRoot;
+import com.nevercome.tabook.modules.book.entity.info.statistics.BookInfoClassStatistics;
+import com.nevercome.tabook.modules.book.service.info.BookInfoClassService;
+import com.nevercome.tabook.modules.book.service.info.BookInfoRootService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author: sun
@@ -14,6 +20,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookInfoAddService extends CrudService<BookInfoAddDao, BookInfoAdd> {
 
+    @Autowired
+    private BookInfoRootService bookInfoRootService;
+    @Autowired
+    private BookInfoClassService bookInfoClassService;
+
     /**
      * 书籍信息的添加
      * 1. 有rootId classId 那么所有的数据都是自动生成的 | 系统中有这本书
@@ -21,6 +32,7 @@ public class BookInfoAddService extends CrudService<BookInfoAddDao, BookInfoAdd>
      * 3. 没有rootId 代表系统中根本没有任何该书的记录 则生成一个新的bookRoot节点和bookClass节点
      */
     @Override
+    @Transactional
     public void save(BookInfoAdd bookInfoAdd) {
         String bookRootId = bookInfoAdd.getBookRootId();
         String bookClassId = bookInfoAdd.getBookRootId();
@@ -29,6 +41,17 @@ public class BookInfoAddService extends CrudService<BookInfoAddDao, BookInfoAdd>
             BookInfoRoot bookInfoRoot = new BookInfoRoot();
             bookInfoRoot.setName(bookInfoAdd.getName());
             bookInfoRoot.setAuthor(bookInfoAdd.getAuthor());
+            bookInfoRoot.setSorts(bookInfoAdd.getSorts());
+            // 同时 保存分类映射
+            bookInfoRootService.save(bookInfoRoot);
+            bookInfoAdd.setBookRootId(bookInfoRoot.getId());
+
+            BookInfoClass bookInfoClass = new BookInfoClass();
+            bookInfoClass.setBookRootId(bookInfoRoot.getId());
+            bookInfoClass.setPress(bookInfoAdd.getPress());
+            bookInfoClass.setYear(bookInfoAdd.getYear());
+            bookInfoClassService.save(bookInfoClass);
+
         } else if (StringUtils.isBlank(bookClassId)) {
 
         } else {

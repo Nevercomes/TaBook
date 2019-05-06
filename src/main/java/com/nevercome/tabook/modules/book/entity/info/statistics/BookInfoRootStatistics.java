76425712d07b02521233889bea064a6f4b81a59e
Nevercome <sunss1903@163.com>
2019-05-06 14:54:14 +0800

    bookInfo的统计数据

--- src/main/java/com/nevercome/tabook/modules/book/entity/info/statistics/BookInfoRootStatistics.java
+++ src/main/java/com/nevercome/tabook/modules/book/entity/info/statistics/BookInfoRootStatistics.java
@@ -1,11 +1,14 @@
 package com.nevercome.tabook.modules.book.entity.info.statistics;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * 可能需要进行对书籍信息进行动静分离
  * 以方便进行缓存
  */
+@Data
 public class BookInfoRootStatistics {
 
     private static final long serialVersionUID = 1L;
@@ -17,43 +20,4 @@ public class BookInfoRootStatistics {
     private Date startTime; // 统计时间
     private Date endTime; // 统计时间
 
-    public int getTotalBorrowed() {
-        return totalBorrowed;
-    }
-
-    public void setTotalBorrowed(int totalBorrowed) {
-        this.totalBorrowed = totalBorrowed;
-    }
-
-    public int getTotalBought() {
-        return totalBought;
-    }
-
-    public void setTotalBought(int totalBought) {
-        this.totalBought = totalBought;
-    }
-
-    public int getCopyNum() {
-        return copyNum;
-    }
-
-    public void setCopyNum(int copyNum) {
-        this.copyNum = copyNum;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
 }

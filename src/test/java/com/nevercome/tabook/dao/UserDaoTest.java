b0afad04795a94324afff8e11ecac798d86da81f
Nevercome <sunss1903@163.com>
2019-05-22 16:13:51 +0800

    fix buy userDao insert

--- src/test/java/com/nevercome/tabook/dao/UserDaoTest.java
+++ src/test/java/com/nevercome/tabook/dao/UserDaoTest.java
@@ -5,6 +5,8 @@ import com.nevercome.tabook.modules.sys.dao.UserDao;
 import com.nevercome.tabook.modules.sys.entity.User;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author: sun
@@ -18,6 +20,17 @@ public class UserDaoTest extends BaseTest {
     @Test
     public void test() {
         System.out.println(userDao.getByLoginName(new User(null, "oGzwE0YrTYfXbAA7cT320xTgNN1w")));
+    }
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testSave() {
+        User user = new User();
+        user.preInsert();
+        user.setName("sun-1");
+        user.setLoginName("sun-1");
+        userDao.insert(user);
     }
 
 }
